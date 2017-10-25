923dd7fc490a8e27fcabc398d7fdd332431c9af9
anbgsl1110 <anbgsl1110@gmail.com>
2017-10-25 20:10:13 +0800

    完成单元测试配置
    1. 引入需要的dll文件
    2. 增加TestBase基础类
    3. 以User为例进行数据操作的测试

---
+++ xiuxian-flume/XiuXianFlume.Tests/TestBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiuXianFlume.Core;
+using XiuXianFlume.Data;
+using XiuXianFlume.Core.Infrastructure;
+using XiuXianFlume.Core.Data;
+
+namespace XiuXianFlume.Tests
+{
+    /// <summary>
+    /// TestBase 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class TestBase
+    {
+        protected XiuXianObjectContext context;
+
+        [TestInitialize]
+        public void Init()
+        {
+            //initialize engine context
+            EngineContext.Initialize(false);
+
+            bool databaseInstalled = DataSettingsHelper.DatabaseIsInstalled();
+
+            context = new XiuXianObjectContext("xiuxianflume");
+            context.Database.Delete();
+            context.Database.Create();
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，该上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试特性
+        //
+        // 编写测试时，可以使用以下附加特性: 
+        //
+        // 在运行类中的第一个测试之前使用 ClassInitialize 运行代码
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在类中的所有测试都已运行之后使用 ClassCleanup 运行代码
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在运行每个测试之前，使用 TestInitialize 来运行代码
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在每个测试运行完之后，使用 TestCleanup 来运行代码
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        /// <summary>
+        /// Persistance test helper
+        /// </summary>
+        /// <typeparam name="T">Entity type</typeparam>
+        /// <param name="entity">Entity</param>
+        /// <param name="disposeContext">A value indicating whether to dispose context</param>
+        protected T SaveAndLoadEntity<T>(T entity, bool disposeContext = true) where T : BaseEntity
+        {
+
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+
+            object id = entity.Id;
+
+            if (disposeContext)
+            {
+                context.Dispose();
+                context = new XiuXianObjectContext("xiuxianflume");
+            }
+
+            var fromDb = context.Set<T>().Find(id);
+            return fromDb;
+        }
+    }
+}
--- xiuxian-flume/XiuXianFlume.Tests/UnitTest1.cs
+++ xiuxian-flume/XiuXianFlume.Tests/UnitTest1.cs
@@ -2,25 +2,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using XiuXianFlume.Core.Infrastructure;
 using XiuXianFlume.Core.Data;
+using XiuXianFlume.Data;
+using XiuXianFlume.Core;
+using XiuXianFlume.Core.Domain;
 
 namespace XiuXianFlume.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 : TestBase
     {
-        [TestInitialize]
-        public void Init()
-        {
-            //initialize engine context
-            EngineContext.Initialize(false);
-
-            bool databaseInstalled = DataSettingsHelper.DatabaseIsInstalled();
-        }
-
         [TestMethod]
         public void TestMethod1()
         {
+            var ca = new User
+            {
+                Name = "Name HHH",
+                Password = "******",
+                Phone = "555",
+                Remark = "哈哈哈"
+            };
 
+            var fromDb = SaveAndLoadEntity(ca);
+            Assert.IsNotNull(fromDb);
+            Assert.AreEqual(fromDb.Name,ca.Name);
         }
     }
 }
